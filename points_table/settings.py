7bfd87c982fda7adf8e69c35f9579196f0e7f87d
AbyvargheseMandapathel <abyvarghesemandapathel@gmail.com>
2023-10-15 00:49:45 +0530

    new

--- points_table/settings.py
+++ points_table/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+from decouple import config
+
+# Read the database URL from the .env file
+DATABASE_URL = config('DATABASE_URL', default='')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,7 +88,7 @@ WSGI_APPLICATION = 'points_table.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse("postgres://pt_iegw_user:HPlGKufAGnzAhPoxOLcGu4To06GruR1d@dpg-ckkg6gua18fc73arf39g-a.singapore-postgres.render.com/pt_iegw")
+    'default': dj_database_url.parse(DATABASE_URL)
 }
 
 
